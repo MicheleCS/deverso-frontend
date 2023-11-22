fee0aa8853dbd3f7854d9121dec2b1230143a806
MicheleCS <michelesouza-@outlook.com>
2023-11-22 10:59:28 -0300

    modify

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,18 @@
+import styles from './Home.module.css'
+import astro from '../../img/astro.png'
+import LinkButton from '../layout/LinkButton'
+
 function Home() {
-  return <h1>Home</h1>
+  return (
+    <section className={styles.home_container}>
+      <h1>Embarque nessa viagem pelo <span>DEVERSO</span></h1>
+      <p>Universo Developer</p>
+      <LinkButton to="/newproject" text="Embarcar" />
+      <div customClass='img_container'>
+        <img src={astro} alt='astro' width="400" height="350"></img>
+      </div>
+    </section>
+  )
 }
 
 export default Home
